ed242b75992eb4fc4b679b0668e2d349994b003d
Emma Rodam <emmarodam@gmail.com>
2024-05-23 13:44:56 +0200

    app.py

--- app.py
+++ app.py
@@ -9,6 +9,55 @@ import re
 import requests
 import json
 
+from bottle import get, post, run, response, Bottle, request
+import git, json
+
+app = Bottle()
+
+@post('/secret_url_for_git_hook')
+def git_update():
+  repo = git.Repo('./uw_exam')
+  origin = repo.remotes.origin
+  repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+  origin.pull()
+  return ""
+
+##############################
+
+@get("/")
+def _():
+  return "Jubii"
+
+##############################
+
+correct_token = "241097"
+
+@get("/crimes")
+def _():
+    token = request.query.get("token", "")
+    if token != correct_token:
+        response.status = 401
+        return "Unauthorized access"
+    try:
+        # Assuming your JSON file is named data.json
+        with open("/home/emmarodam/uw_exam/crime.json", "r") as f:
+            data = json.load(f)
+        response.content_type = 'application/json'
+        return json.dumps(data)
+    except Exception as e:
+        return f"Error: {e}"
+
+if __name__ == "__main__":
+    run(app, host='0.0.0.0', port=8080, debug=True)
+
+##############################
+try:
+  import production
+  application = default_app()
+except Exception as ex:
+  print("Running local server")
+  run(host="127.0.0.1", port=80, debug=True, reloader=True)
+
 ##############################
 @get("/app.css")
 def _():
